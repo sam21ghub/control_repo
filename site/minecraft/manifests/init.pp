ec68bbdd1cd133afbf465f226b2d4a1766cf73dc
Sanjay Rajwadi <sanjay.rajwadi21@gmail.com>
2021-05-13 21:54:24 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,5 @@
 class minecraft {
-  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
   $install_dir = '/opt/minecraft'
 }{
   file {$install_dir:
